2753f6ed93010b5b43811332572127831a778a36
AbdulRahimFarooqui <abdurraheemfarooqui@gmail.com>
2020-07-05 12:31:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Assalaamualaikum, Dear Zone Zone!!! <br/> <code></code>
-        </p>
-        <h1>Say Mashaa' Allah</h1>
-        <p><b>Mujhay khud nahin pata ye kaisay bana, lekin aap idher say kuch dekh saktay hain Inshaa' Allah....</b></p>
-        <a
-
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lekin click here to know SOMETHING
-        </a>
+        <h1>Hello World</h1>
       </header>
     </div>
   );
